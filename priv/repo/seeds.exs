ddd1471720ca802f942737efcb5c9684b696bfdf
Radek Szymczyszyn <lavrin@gmail.com>
2024-07-17 15:11:08 +0200

    Seed some group members

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,32 +13,41 @@
 alias Staek.Repo
 alias Staek.Accounts.User
 alias Staek.Expenses.Group
+alias Staek.Expenses.GroupMembers
 
-user1 = %{
+user1_params = %{
   email: "user1@example.com",
   password: "user1secret_whichmustbeatleastNcharacterslong"
 }
 
-user2 = %{
+user2_params = %{
   email: "user2@example.com",
   password: "user2secret_whichmustbeatleastNcharacterslong"
 }
 
-user3 = %{
+user3_params = %{
   email: "user3@example.com",
   password: "user3secret_whichmustbeatleastNcharacterslong"
 }
 
-Enum.each([user1, user2, user3], fn user ->
+[user1, _user2, user3] = Enum.map([user1_params, user2_params, user3_params], fn user ->
   %User{}
   |> User.registration_changeset(user)
   |> Repo.insert!()
 end)
 
-group1 = %{
+group1_params = %{
   name: "Last night out"
 }
 
-%Group{}
-|> Group.changeset(group1)
-|> Repo.insert!()
+group1 = %Group{}
+         |> Group.changeset(group1_params)
+         |> Repo.insert!()
+
+memberships = [{group1.id, user1.id}, {group1.id, user3.id}]
+
+Enum.each(memberships, fn {group_id, user_id} ->
+  %GroupMembers{}
+  |> GroupMembers.changeset(%{group_id: group_id, user_id: user_id})
+  |> Repo.insert!()
+end)
