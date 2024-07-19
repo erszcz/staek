86556a42469d70b9229dccda236059d5a9924f95
Radek Szymczyszyn <lavrin@gmail.com>
2024-07-19 13:12:27 +0200

    Depend on decimal

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Staek.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:decimal, "~> 2.1"}
     ]
   end
 
