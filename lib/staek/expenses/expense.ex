de8a876bc7984a3ae5434d5c7f9bbec947bd0686
Radek Szymczyszyn <lavrin@gmail.com>
2024-07-19 14:34:51 +0200

    Add credit and debit references to expenses

--- lib/staek/expenses/expense.ex
+++ lib/staek/expenses/expense.ex
@@ -2,12 +2,16 @@ defmodule Staek.Expenses.Expense do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Staek.Expenses.Credit
+  alias Staek.Expenses.Debit
   alias Staek.Expenses.Group
 
   schema "expenses" do
     field :name, :string
     field :total, :decimal
     belongs_to :group, Group
+    has_many :credits, Credit
+    has_many :debits, Debit
 
     timestamps(type: :utc_datetime)
   end
