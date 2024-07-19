22b839aefffbfa866363b8e2d6b1aef278c50b46
Radek Szymczyszyn <lavrin@gmail.com>
2024-07-19 14:35:09 +0200

    Add IEx aliases

--- .iex.exs
+++ .iex.exs
@@ -1,7 +1,9 @@
 alias Staek.Accounts
 alias Staek.Accounts.User
-alias Staek.Expenses.Expense
 alias Staek.Expenses
+alias Staek.Expenses.Credit
+alias Staek.Expenses.Debit
+alias Staek.Expenses.Expense
 alias Staek.Expenses.Group
 alias Staek.Expenses.GroupMembers
 alias Staek.Repo
