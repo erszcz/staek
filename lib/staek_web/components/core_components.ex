b238ce461bf471aa9f4112bc3cf8b74c7e11ec4f
Radek Szymczyszyn <lavrin@gmail.com>
2024-07-22 10:52:28 +0200

    Make login form background the same as the rest of the app

--- lib/staek_web/components/core_components.ex
+++ lib/staek_web/components/core_components.ex
@@ -202,7 +202,7 @@ defmodule StaekWeb.CoreComponents do
   def simple_form(assigns) do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
-      <div class="mt-10 space-y-8 bg-white">
+      <div class="mt-10 space-y-8 bg-gray-100">
         <%= render_slot(@inner_block, f) %>
         <div :for={action <- @actions} class="mt-2 flex items-center justify-between gap-6">
           <%= render_slot(action, f) %>
