adf722c156dae825a2696462aae25c94608a53a0
Radek Szymczyszyn <lavrin@gmail.com>
2024-08-01 16:39:19 +0200

    Depend on Elixir Desktop

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule StaekDesktop.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:desktop, github: "elixir-desktop/desktop"}
     ]
   end
 
