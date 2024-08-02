7fbd4b250929bb36f8afbdcc821878b318d5f710
Radek Szymczyszyn <lavrin@gmail.com>
2024-08-02 14:45:03 +0200

    Depend on bcrypt

--- apps/staek/mix.exs
+++ apps/staek/mix.exs
@@ -36,6 +36,7 @@ defmodule Staek.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:dns_cluster, "~> 0.1.1"},
       {:phoenix_pubsub, "~> 2.1"},
       {:ecto_sql, "~> 3.10"},
