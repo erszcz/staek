1ff05023019dbdf757453e9ee52c0eca820c402f
Radek Szymczyszyn <lavrin@gmail.com>
2024-08-06 16:18:58 +0200

    Copy over .iex.exs to staek_{desktop,web}

---
+++ apps/staek_web/.iex.exs
@@ -0,0 +1,10 @@
+alias Staek.Accounts
+alias Staek.Accounts.User
+alias Staek.Currencies
+alias Staek.Expenses
+alias Staek.Expenses.Credit
+alias Staek.Expenses.Debit
+alias Staek.Expenses.Expense
+alias Staek.Expenses.Group
+alias Staek.Expenses.GroupMembers
+alias Staek.Repo
