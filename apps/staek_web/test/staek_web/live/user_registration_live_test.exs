f4b979e316350297f9046ad69eaecfa2d259bab9
Radek Szymczyszyn <lavrin@gmail.com>
2024-08-13 12:23:51 +0200

    Fix user_registration_live_test.exs:40 test

--- apps/staek_web/test/staek_web/live/user_registration_live_test.exs
+++ apps/staek_web/test/staek_web/live/user_registration_live_test.exs
@@ -41,7 +41,8 @@ defmodule StaekWeb.UserRegistrationLiveTest do
       {:ok, lv, _html} = live(conn, ~p"/users/register")
 
       email = unique_user_email()
-      form = form(lv, "#registration_form", user: valid_user_attributes(email: email))
+      user_attrs = valid_user_attributes(email: email)
+      form = form(lv, "#registration_form", user: user_attrs)
       render_submit(form)
       conn = follow_trigger_action(form, conn)
 
@@ -50,7 +51,7 @@ defmodule StaekWeb.UserRegistrationLiveTest do
       # Now do a logged in request and assert on the menu
       conn = get(conn, "/")
       response = html_response(conn, 200)
-      assert response =~ email
+      assert response =~ user_attrs.name
       assert response =~ "Settings"
       assert response =~ "Log out"
     end
