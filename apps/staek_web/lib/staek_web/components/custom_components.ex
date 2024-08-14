7ca01fd44bdfbf064af9c715e852defac577c62f
Radek Szymczyszyn <lavrin@gmail.com>
2024-08-14 15:07:16 +0200

    Use Flowbite .dropdown component

--- apps/staek_web/lib/staek_web/components/custom_components.ex
+++ apps/staek_web/lib/staek_web/components/custom_components.ex
@@ -28,4 +28,33 @@ defmodule StaekWeb.CustomComponents do
     </div>
     """
   end
+
+  attr :text, :any, required: true
+  attr :items, :any, required: true
+
+  def dropdown(assigns) do
+    ~H"""
+    <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" class="w-44 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+      <%= @text %>
+      <svg class="ms-auto w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+      </svg>
+    </button>
+
+    <!-- Dropdown menu -->
+    <div id="dropdownHover" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+      <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
+        <li :for={item <- @items}>
+          <.link
+            href={item.href}
+            method={item[:method] || "get"}
+            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            <%= item.text %>
+          </.link>
+        </li>
+        </ul>
+    </div>
+    """
+  end
 end
