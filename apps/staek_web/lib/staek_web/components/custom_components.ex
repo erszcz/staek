c37454676242795fa901cc459884d3a369237aad
Radek Szymczyszyn <lavrin@gmail.com>
2024-10-23 18:31:43 +0200

    Adjust user menu visibility on the home page

--- apps/staek_web/lib/staek_web/components/custom_components.ex
+++ apps/staek_web/lib/staek_web/components/custom_components.ex
@@ -37,8 +37,8 @@ defmodule StaekWeb.CustomComponents do
     <button
       id="dropdownHoverButton"
       data-dropdown-toggle="dropdownHover"
-      data-dropdown-trigger="hover"
-      class="w-44 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      data-dropdown-trigger="click"
+      class="w-44 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-white"
       type="button"
     >
       <%= @text %>
